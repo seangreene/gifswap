c36f31aa16d7486c2099972854c03ecd21637c11
seangreene <seangreene@edt-c02hh27qdv7p.usa.tribune.com>
2015-01-30 22:46:52 -0800

    Replace an image with a GIF

---
+++ gifswap.js
@@ -0,0 +1,27 @@
+	//script to restart the animation
+    $(function(){
+    var image = new Image();
+       image.src='https://s3.amazonaws.com/gifs-seangreene/future-home-design-animation-final.gif';
+     $('#home-image').click(function(){
+       $(this).attr('src',image.src);
+     }); 
+
+ 	});
+
+    $(function(){
+    var image = new Image();
+       image.src='https://s3.amazonaws.com/gifs-seangreene/future-health-animation-final.gif';
+     $('#health-image').click(function(){
+       $(this).attr('src',image.src);
+     }); 
+
+ 	});
+
+    $(function(){
+    var image = new Image();
+       image.src='https://s3.amazonaws.com/gifs-seangreene/future-food-animation-final.gif';
+     $('#food-image').click(function(){
+       $(this).attr('src',image.src);
+     }); 
+
+ 	});
